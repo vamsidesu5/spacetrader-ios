852bff931ab2bb9751a67f051003b93766f65160
vamsidesu5 <vamsidesu5@gmail.com>
2019-04-19 22:08:25 -0400

    elements for new player

---
+++ spacetrader/PlayerController.swift
@@ -0,0 +1,47 @@
+
+import UIKit
+
+class PlayerController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate{
+    
+    @IBOutlet weak var pilotSlider: UISlider!
+    @IBOutlet weak var engineerSlider: UISlider!
+    @IBOutlet weak var traderSlider: UISlider!
+    @IBOutlet weak var fighterSlider: UISlider!
+    @IBOutlet weak var pilotLabel: UILabel!
+    @IBOutlet weak var engineerLabel: UILabel!
+    @IBOutlet weak var traderLabel: UILabel!
+    @IBOutlet weak var fighterLabel: UILabel!
+    @IBOutlet weak var diffPicker: UIPickerView!
+    let pickerData = ["Easy","Normal","Hard"]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func pilotValueChanger(_ sender: UISlider) {
+        let curr = Int(sender.value);
+        pilotLabel.text = "\(curr)"
+    }
+    @IBAction func enginerValueChanger(_ sender: UISlider) {
+        let curr = Int(sender.value);
+        engineerLabel.text = "\(curr)"
+    }
+    @IBAction func traderValueChanger(_ sender: UISlider) {
+        let curr = Int(sender.value);
+        traderLabel.text = "\(curr)"
+    }
+    
+    @IBAction func fighterValueChanger(_ sender: UISlider) {
+        let curr = Int(sender.value);
+        fighterLabel.text = "\(curr)"
+    }
+}
--- spacetrader/ViewController.swift
+++ spacetrader/ViewController.swift
@@ -5,16 +5,15 @@
 //  Created by vamsi on 4/19/19.
 //  Copyright © 2019 com.vamsi. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var newGameButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
